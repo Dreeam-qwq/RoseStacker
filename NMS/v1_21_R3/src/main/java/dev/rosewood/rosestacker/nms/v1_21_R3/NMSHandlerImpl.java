83bef252a1858beba483016610eea6e323d7934b
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2025-03-15 19:06:16 -0400

    Add missing impl

--- NMS/v1_21_R3/src/main/java/dev/rosewood/rosestacker/nms/v1_21_R3/NMSHandlerImpl.java
+++ NMS/v1_21_R3/src/main/java/dev/rosewood/rosestacker/nms/v1_21_R3/NMSHandlerImpl.java
@@ -61,6 +61,7 @@ import net.minecraft.world.entity.monster.Strider;
 import net.minecraft.world.entity.monster.Zombie;
 import net.minecraft.world.entity.raid.Raider;
 import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.item.component.ItemContainerContents;
 import net.minecraft.world.item.trading.MerchantOffers;
 import net.minecraft.world.level.BaseSpawner;
 import net.minecraft.world.level.ClipContext;
@@ -501,6 +502,21 @@ public class NMSHandlerImpl implements NMSHandler {
     @Override
     public EntityDeathEvent createAsyncEntityDeathEvent(@NotNull LivingEntity what, @NotNull List<ItemStack> drops, int droppedExp) {
         return new AsyncEntityDeathEventImpl(what, drops, droppedExp);
+    }
+
+    @Override
+    public List<ItemStack> getBoxContents(Item item) {
+        net.minecraft.world.item.ItemStack itemStack = CraftItemStack.asNMSCopy(item.getItemStack());
+        ItemContainerContents contents = itemStack.set(DataComponents.CONTAINER, ItemContainerContents.EMPTY);
+
+        if (contents != null) {
+            return contents.stream()
+                    .filter(x -> !x.isEmpty())
+                    .map(CraftItemStack::asBukkitCopy)
+                    .toList();
+        }
+
+        return new ArrayList<>();
     }
 
     @Override
